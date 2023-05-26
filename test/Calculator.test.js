0a2fba62b149d263dcec8f4fb67b2960348c310b
Philoem <philoem24@gmail.com>
2023-05-26 15:56:46 +0200

    test: add test

---
+++ test/Calculator.test.js
@@ -0,0 +1,24 @@
+const Calculator = artifacts.require("Calculator");
+
+contract('Calculator', async function(accounts) {
+  it('adds two numbers', async function() {
+    const contract = await Calculator.deployed();
+    const result = await contract.add(1, 2, {from: accounts[0]});
+    assert.equal(result, 3, 'Not equal to 3');
+  })
+  it('subtracts two numbers', async function() {
+    const contract = await Calculator.deployed();
+    const result = await contract.subtract(1, 2, {from: accounts[0]});
+    assert.equal(result, -1, 'Not equal to -1');
+  })
+  it('multiplies two numbers', async function() {
+    const contract = await Calculator.deployed();
+    const result = await contract.multiply(1, 2, {from: accounts[0]});
+    assert.equal(result, 2, 'Not equal to 2');
+  })
+  it('divides two numbers', async function() {
+    const contract = await Calculator.deployed();
+    const result = await contract.divide(1, 2, {from: accounts[0]});
+    assert.equal(result, 0.5, 'Not equal to 0.5');
+  })
+})
